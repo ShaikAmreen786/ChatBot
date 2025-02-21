0dd4f78565de4a6ae1c83bba39a6800028c32412
Srinivasa Chari <srinivaschari6268@gmail.com>
2025-02-21 14:25:52 +0530

    updated

--- src/components/RolePredictionForm.js
+++ src/components/RolePredictionForm.js
@@ -110,7 +110,7 @@ export default function RolePredictionForm() {
         ];
 
         try {
-            const response = await fetch('http://127.0.0.1:5000/predict', {
+            const response = await fetch('http://10.10.88.136:5000/predict', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
